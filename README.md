69d6a2637acb90eadc1ccf000f008c3772ea53c5
AnyaB <aubogomolova>
2021-10-03 14:49:22 +0300

    first load

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MemcLoad
+
+`python memc_load.py -l ./log_memc.py --dry True --pattern ./data_test/*.tsv.gz`
---
+++ memc_load.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import gzip
+import sys
+import glob
+import logging
+import collections
+import threading
+from optparse import OptionParser
+from queue import Queue
+# brew install protobuf
+# protoc  --python_out=. ./appsinstalled.proto
+# pip install protobuf
+import appsinstalled_pb2
+# pip install python-memcached
+from pymemcache.client.base import Client
+
+NORMAL_ERR_RATE = 0.01
+AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
+
+
+def dot_rename(path):
+    head, fn = os.path.split(path)
+    # atomic in most cases
+    os.rename(path, os.path.join(head, "." + fn))
+
+
+def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
+    ua = appsinstalled_pb2.UserApps()
+    ua.lat = appsinstalled.lat
+    ua.lon = appsinstalled.lon
+    key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
+    ua.apps.extend(appsinstalled.apps)
+    packed = ua.SerializeToString()
+    # @TODO persistent connection
+    # @TODO retry and timeouts!
+    try:
+        if dry_run:
+            logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
+            print('{} successfully load {} -> {}'.format(threading.current_thread(), key, str(ua)))
+        else:
+            memc = Client(memc_addr)
+            memc.set(key, packed)
+    except Exception as e:
+        logging.exception("Cannot write to memc %s: %s" % (memc_addr, e))
+        return False
+    return True
+
+
+def parse_appsinstalled(line):
+    line_parts = line.strip().split("\t")
+    if len(line_parts) < 5:
+        return
+    dev_type, dev_id, lat, lon, raw_apps = line_parts
+    if not dev_type or not dev_id:
+        return
+    try:
+        apps = [int(a.strip()) for a in raw_apps.split(",")]
+    except ValueError:
+        apps = [int(a.strip()) for a in raw_apps.split(",") if a.isidigit()]
+        logging.info("Not all user apps are digits: `%s`" % line)
+    try:
+        lat, lon = float(lat), float(lon)
+    except ValueError:
+        logging.info("Invalid geo coords: `%s`" % line)
+    return AppsInstalled(dev_type, dev_id, lat, lon, apps)
+
+
+def process_file(file, device_memc, options):
+    processed = errors = 0
+    fd = gzip.open(file)
+    for line in fd:
+        line = line.decode('utf-8').strip()
+        print('{} took line {}'.format(threading.current_thread(), line))
+        if not line:
+            continue
+        appsinstalled = parse_appsinstalled(line)
+        print('{} took state {}'.format(threading.current_thread(), appsinstalled))
+        if not appsinstalled:
+            errors += 1
+            continue
+        memc_addr = device_memc.get(appsinstalled.dev_type)
+        if not memc_addr:
+            errors += 1
+            logging.error("Unknow device type: %s" % appsinstalled.dev_type)
+            continue
+        ok = insert_appsinstalled(memc_addr, appsinstalled, options.dry)
+        print('{} successfully load {}'.format(threading.current_thread(), ok))
+        if ok:
+            processed += 1
+        else:
+            errors += 1
+        print('{} processed number {} errors number {}'.format(threading.current_thread(), processed, errors))
+    if processed:
+        err_rate = float(errors) / processed
+        if err_rate < NORMAL_ERR_RATE:
+            logging.info("Acceptable error rate (%s). Successfull load" % err_rate)
+        else:
+            logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
+    fd.close()
+
+
+class Worker(threading.Thread):
+    def __init__(self, queue, device_memc, options, lock):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.lock = lock
+        self.memc_dict = device_memc
+        self.options = options
+
+    def run(self):
+        while True:
+            file_ex = self.queue.get()
+            try:
+                process_file(file_ex, self.memc_dict, self.options)
+                with self.lock:
+                    print(f'Th: {self.name} treat {file_ex} file')
+                    dot_rename(file_ex)
+            except Exception as e:
+                logging.info("Exception {} occurred with processing file {}".format(e, file_ex))
+            finally:
+                self.queue.task_done()
+
+
+def main(options):
+    device_memc = {
+        "idfa": options.idfa,
+        "gaid": options.gaid,
+        "adid": options.adid,
+        "dvid": options.dvid,
+    }
+
+    files_queue = Queue()
+    for fn in glob.iglob(options.pattern):
+        files_queue.put(fn)
+
+    if files_queue.empty():
+        logging.info("There are no files to load")
+    else:
+        lock = threading.Lock()
+        for _ in range(os.cpu_count()):
+            th = Worker(files_queue, device_memc, options, lock)
+            th.daemon = True
+            th.start()
+        files_queue.join()
+
+
+def prototest():
+    sample = "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
+    for line in sample.splitlines():
+        dev_type, dev_id, lat, lon, raw_apps = line.strip().split("\t")
+        apps = [int(a) for a in raw_apps.split(",") if a.isdigit()]
+        lat, lon = float(lat), float(lon)
+        ua = appsinstalled_pb2.UserApps()
+        ua.lat = lat
+        ua.lon = lon
+        ua.apps.extend(apps)
+        packed = ua.SerializeToString()
+        unpacked = appsinstalled_pb2.UserApps()
+        unpacked.ParseFromString(packed)
+        assert ua == unpacked
+
+
+if __name__ == '__main__':
+    op = OptionParser()
+    op.add_option("-t", "--test", action="store_true", default=False)
+    op.add_option("-l", "--log", action="store", default=None)
+    op.add_option("--dry", action="store_true", default=False)
+    op.add_option("--pattern", action="store", default="/data/appsinstalled/*.tsv.gz")
+    op.add_option("--idfa", action="store", default="127.0.0.1:33013")
+    op.add_option("--gaid", action="store", default="127.0.0.1:33014")
+    op.add_option("--adid", action="store", default="127.0.0.1:33015")
+    op.add_option("--dvid", action="store", default="127.0.0.1:33016")
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    if opts.test:
+        prototest()
+        sys.exit(0)
+
+    logging.info("Memc loader started with options: %s" % opts)
+    try:
+        main(opts)
+    except Exception as e:
+        logging.exception("Unexpected error: %s" % e)
+        sys.exit(1)
